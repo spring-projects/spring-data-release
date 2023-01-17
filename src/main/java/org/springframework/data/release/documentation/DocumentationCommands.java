627870ff711575e79a86b4d93bfb7d4c4bf8de33
Christoph Strobl <cstrobl@vmware.com>
2023-01-17 11:20:31 +0100

    Use logger and add project and error options

--- src/main/java/org/springframework/data/release/documentation/DocumentationCommands.java
+++ src/main/java/org/springframework/data/release/documentation/DocumentationCommands.java
@@ -17,12 +17,18 @@ package org.springframework.data.release.documentation;
 
 import static org.springframework.data.release.model.Projects.*;
 
+import java.util.Arrays;
+
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.release.CliComponent;
 import org.springframework.data.release.TimedCommand;
 import org.springframework.data.release.cli.StaticResources;
+import org.springframework.data.release.documentation.DocumentationOperations.ReportFlags;
+import org.springframework.data.release.model.ModuleIteration;
+import org.springframework.data.release.model.Project;
 import org.springframework.data.release.model.TrainIteration;
+import org.springframework.data.release.utils.Logger;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 
@@ -34,20 +40,28 @@ import org.springframework.shell.core.annotation.CliOption;
 public class DocumentationCommands extends TimedCommand {
 
 	private final @NonNull DocumentationOperations operations;
+	private final @NonNull Logger logger;
 
 	@CliCommand("docs check-links")
-	public void checkLinks(@CliOption(key = "", mandatory = true) TrainIteration iteration) {
+	public void checkLinks(@CliOption(key = "", mandatory = true) TrainIteration iteration, @CliOption(key = "project", mandatory = false) Project project, @CliOption(key = "local", mandatory = false, unspecifiedDefaultValue = "false") boolean preview, @CliOption(key = "report", mandatory = false) String options) {
 
 		iteration.getModulesExcept(BUILD, BOM, COMMONS).forEach(module -> {
 
-			StaticResources resources = new StaticResources(module);
-			checkLinks(resources.getDocumentationUrl());
+			if (project == null || module.getProject().getName().equals(project.getName())) {
+				checkLinks(module, preview, options);
+			}
 		});
 	}
 
-	@CliCommand("docs check-links-url")
-	public void checkLinks(@CliOption(key = "", mandatory = true) String url) {
-		operations.checkDocumentation(url).prettyPrint();
+	public void checkLinks(ModuleIteration module, boolean preview, String options) {
+
+		if (preview) {
+			throw new RuntimeException("to be implemented");
+		} else {
+			StaticResources resources = new StaticResources(module);
+			String result = operations.checkDocumentation(resources.getDocumentationUrl()).prettyPrint(Arrays.stream(options.split(",")).map(ReportFlags::valueOf).toArray(ReportFlags[]::new));
+			logger.log(module, "%s Documentation Link Statistic:\r\n%s", module, result);
+		}
 	}
 }
 
--- src/main/java/org/springframework/data/release/documentation/DocumentationOperations.java
+++ src/main/java/org/springframework/data/release/documentation/DocumentationOperations.java
@@ -23,41 +23,76 @@ import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.Ansi.Color;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.data.release.utils.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 /**
  * @author Christoph Strobl
  */
 @Component
+@RequiredArgsConstructor
 class DocumentationOperations {
+
+	private final @NonNull Logger logger;
 
 	PageStats checkDocumentation(String url) {
 		PageStats stats = new PageStats();
-		stats.links(new LinkChecker().inspect(url));
+		stats.links(new LinkChecker(logger).inspect(url));
 		return stats;
+	}
+
+	enum ReportFlags {
+
+		ERROR(1), REDIRECT(2), OK(4), ALL(8);
+
+		int bin;
+
+		ReportFlags(int bin) {
+			this.bin = bin;
+		}
+
+		static int flagsOf(ReportFlags... flags) {
+			int value = 0;
+			for (ReportFlags opt : flags) {
+				value = value | opt.bin;
+			}
+			return value;
+		}
 	}
 
 	static class PageStats {
 
+		public static final int ERROR = 1;  // Binary 00001
+		public static final int REDIRECT = 2;  // Binary 00010
+		public static final int OK = 4;  // Binary 00100
+		public static final int ALL = 8;  // Binary 01000
+
 		LinkStats linkStats;
 
-		void prettyPrint() {
+		String prettyPrint(ReportFlags... options) {
+
 			if (linkStats != null) {
-				linkStats.prettyPrint();
+				return linkStats.prettyPrint(options);
 			}
+			return "PageStats n/a";
 		}
 
 		void links(LinkStats linkStats) {
 			this.linkStats = linkStats;
 		}
 	}
+
 
 	static class LinkStats {
 
@@ -67,9 +102,33 @@ class DocumentationOperations {
 			return resultMap.computeIfAbsent(key, mappingFunction);
 		}
 
-		void prettyPrint() {
+		int size() {
+			return resultMap.size();
+		}
 
-			resultMap.entrySet().stream().sorted(Comparator.comparingInt(o -> o.getValue().value())).forEach(entry -> {
+		String prettyPrint() {
+			return prettyPrint(ReportFlags.ALL);
+		}
+
+		String prettyPrint(ReportFlags... options) {
+
+			int flags = ObjectUtils.isEmpty(options) ? ReportFlags.flagsOf(ReportFlags.ALL) : ReportFlags.flagsOf(options);
+
+			return resultMap.entrySet().stream().filter(entry -> {
+				if ((flags & ReportFlags.ALL.bin) == ReportFlags.ALL.bin) {
+					return true;
+				}
+				if (entry.getValue().is2xxSuccessful() && (flags & ReportFlags.OK.bin) == ReportFlags.OK.bin) {
+					return true;
+				}
+				if (entry.getValue().is3xxRedirection() && (flags & ReportFlags.REDIRECT.bin) == ReportFlags.REDIRECT.bin) {
+					return true;
+				}
+				if (entry.getValue().is4xxClientError() && (flags & ReportFlags.ERROR.bin) == ReportFlags.ERROR.bin) {
+					return true;
+				}
+				return false;
+			}).sorted(Comparator.comparingInt(o -> o.getValue().value())).map(entry -> {
 				Ansi ansi = Ansi.ansi();
 				if (entry.getValue().is2xxSuccessful()) {
 					ansi.fg(Color.GREEN);
@@ -78,17 +137,20 @@ class DocumentationOperations {
 				else if (entry.getValue().is3xxRedirection()) {
 					ansi.fg(Color.YELLOW);
 				}
-				System.out.println(ansi.a(entry.getValue()).fg(Color.DEFAULT).a(": " + entry.getKey()).toString());
-			});
+				return ansi.a(entry.getValue()).fg(Color.DEFAULT).a(": " + entry.getKey()).toString();
+			}).collect(Collectors.joining("\r\n"));
 		}
 	}
 
 
+	@RequiredArgsConstructor
 	static class LinkChecker {
+
+		private final Logger logger;
 
 		LinkStats inspect(String url) throws RuntimeException {
 
-			System.out.println("Collecting links from: " + url);
+			logger.log("", "Collecting links from: %s", url);
 
 			Document doc = null;
 			try {
@@ -106,8 +168,7 @@ class DocumentationOperations {
 
 			Elements links = doc.select("a[href]"); // a with href
 
-			System.out.println("Found " + links.size() + " links.");
-			System.out.print("Processing ");
+			logger.log("", "Found %s links.", links.size());
 
 			LinkStats stats = new LinkStats();
 
@@ -117,7 +178,8 @@ class DocumentationOperations {
 				}
 				checkUrl(link.attr("href"), stats);
 			});
-			System.out.println();
+
+			logger.log("", "Analyzed %s external links.", stats.size());
 
 			return stats;
 		}
@@ -125,7 +187,6 @@ class DocumentationOperations {
 		private void checkUrl(String url, LinkStats stats) {
 
 			stats.computeIfAbsent(url, key -> {
-				System.out.print(".");
 				try {
 					HttpURLConnection connection = (HttpURLConnection) new URL(key).openConnection();
 					connection.setRequestMethod("HEAD");
