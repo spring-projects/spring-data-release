e4b19025cc5bc989d6c6f4d3317e246704f298d8
Mark Paluch <mark.paluch@broadcom.com>
2024-11-15 14:08:11 +0100

    Reduce parallelity.

--- src/main/java/org/springframework/data/release/utils/ExecutorConfiguration.java
+++ src/main/java/org/springframework/data/release/utils/ExecutorConfiguration.java
@@ -43,7 +43,7 @@ class ExecutorConfiguration {
 	public ThreadPoolExecutorFactoryBean threadPoolExecutorFactoryBean() {
 
 		int processors = Runtime.getRuntime().availableProcessors();
-		int threadCount = Math.max(4, processors - 4);
+		int threadCount = Math.max(2, processors - 4);
 		log.info(String.format("Setting up Executor Service with %d Threads, %d processors", threadCount, processors));
 
 		ThreadPoolExecutorFactoryBean scheduler = new ThreadPoolExecutorFactoryBean();
