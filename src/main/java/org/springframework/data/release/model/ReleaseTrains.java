20cf5e00c3844987f960e5c41f8126a38dfa4b53
Mark Paluch <mark.paluch@broadcom.com>
2025-06-13 11:22:20 +0200

    Switch 2024.0 release train to commercial support.

--- src/main/java/org/springframework/data/release/model/ReleaseTrains.java
+++ src/main/java/org/springframework/data/release/model/ReleaseTrains.java
@@ -34,31 +34,19 @@ public class ReleaseTrains {
 
 	static {
 
-		CODD = codd()
+		CODD = codd().withSupportStatus(SupportStatus.EOL);
+		DIJKSTRA = dijkstra().withSupportStatus(SupportStatus.EOL);
+		EVANS = DIJKSTRA.next("Evans", Transition.MINOR).withSupportStatus(SupportStatus.EOL);
+		FOWLER = EVANS.next("Fowler", Transition.MINOR).withSupportStatus(SupportStatus.EOL);
+		GOSLING = FOWLER.next("Gosling", Transition.MINOR, new Module(KEY_VALUE, "1.0"))
 				.withSupportStatus(SupportStatus.EOL);
-		DIJKSTRA = dijkstra()
-				.withSupportStatus(SupportStatus.EOL);
-		EVANS = DIJKSTRA.next("Evans", Transition.MINOR)
-				.withSupportStatus(SupportStatus.EOL);
-		FOWLER = EVANS.next("Fowler", Transition.MINOR)
-				.withSupportStatus(SupportStatus.EOL);
-		GOSLING = FOWLER.next("Gosling", Transition.MINOR,
-				new Module(KEY_VALUE, "1.0"))
-				.withSupportStatus(SupportStatus.EOL);
-		HOPPER = GOSLING.next("Hopper", Transition.MINOR,
-				new Module(SOLR, "2.0"),
-				new Module(ENVERS, "1.0"),
-				new Module(NEO4J, "4.1"),
-				new Module(COUCHBASE, "2.1"),
-				new Module(ELASTICSEARCH, "2.0"))
+		HOPPER = GOSLING.next("Hopper", Transition.MINOR, new Module(SOLR, "2.0"), new Module(ENVERS, "1.0"),
+				new Module(NEO4J, "4.1"), new Module(COUCHBASE, "2.1"), new Module(ELASTICSEARCH, "2.0"))
 				.withSupportStatus(SupportStatus.EOL);
 
-		INGALLS = HOPPER.next("Ingalls", Transition.MINOR,
-				new Module(LDAP, "1.0"))
-				.withSupportStatus(SupportStatus.EOL);
+		INGALLS = HOPPER.next("Ingalls", Transition.MINOR, new Module(LDAP, "1.0")).withSupportStatus(SupportStatus.EOL);
 
-		KAY = INGALLS.next("Kay", Transition.MAJOR,
-				new Module(GEODE, "2.0")) //
+		KAY = INGALLS.next("Kay", Transition.MAJOR, new Module(GEODE, "2.0")) //
 				.withSupportStatus(SupportStatus.EOL);
 
 		LOVELACE = KAY.next("Lovelace", Transition.MINOR, //
@@ -66,8 +54,7 @@ public class ReleaseTrains {
 				new Module(SOLR, "4.0")) //
 				.withSupportStatus(SupportStatus.EOL);
 
-		MOORE = LOVELACE.next("Moore", Transition.MINOR)
-				.withSupportStatus(SupportStatus.EOL);
+		MOORE = LOVELACE.next("Moore", Transition.MINOR).withSupportStatus(SupportStatus.EOL);
 
 		NEUMANN = MOORE.next("Neumann", Transition.MINOR, //
 				new Module(COUCHBASE, "4.0"), //
@@ -82,8 +69,7 @@ public class ReleaseTrains {
 		OCKHAM = NEUMANN.next("Ockham", Transition.MINOR, //
 				new Module(BOM, "2020.0.0"), //
 				new Module(NEO4J, "6.0") //
-		).withIterations(Train.Iterations.DEFAULT)
-				.withCalver("2020.0") //
+		).withIterations(Train.Iterations.DEFAULT).withCalver("2020.0") //
 				.withSupportStatus(SupportStatus.EOL);
 
 		PASCAL = OCKHAM.next("Pascal", Transition.MINOR) //
@@ -118,7 +104,8 @@ public class ReleaseTrains {
 				.withSupportStatus(SupportStatus.COMMERCIAL);
 
 		W = VAUGHAN.next("W", Transition.MINOR) //
-				.withCalver("2024.0");
+				.withCalver("2024.0") //
+				.withSupportStatus(SupportStatus.COMMERCIAL);
 
 		X = W.next("X", Transition.MINOR) //
 				.withCalver("2024.1");
@@ -142,7 +129,6 @@ public class ReleaseTrains {
 		Module mongoDb = new Module(MONGO_DB, "1.4");
 		Module neo4j = new Module(NEO4J, "3.0");
 		Module solr = new Module(SOLR, "1.1");
-
 		Module rest = new Module(REST, "2.0");
 
 		return new Train("Codd", build, commons, jpa, mongoDb, neo4j, solr, rest);
