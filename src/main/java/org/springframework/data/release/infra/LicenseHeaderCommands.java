spring-projects/spring-data-release

commit e0dbf5e4ba38cd0ef5f6a9f9cd1919eb32c6ce1d

Add Broadcom license header update.
This command adds the ability to update license headers
for commercially supported projects which switches the
headers from Apache 2.0 to Broadcom foundation license.

Signed-off-by: Chris Bono <chris.bono@broadcom.com>
Closes #116

src/main/java/org/springframework/data/release/infra/LicenseHeaderCommands.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the original author or authors.
+ * Copyright 2022-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,6 +48,7 @@ import org.springframework.data.release.issues.TicketOperations;
 import org.springframework.data.release.model.ModuleIteration;
 import org.springframework.data.release.model.Project;
 import org.springframework.data.release.model.Projects;
+import org.springframework.data.release.model.SupportStatus;
 import org.springframework.data.release.model.SupportedProject;
 import org.springframework.data.release.model.TrainIteration;
 import org.springframework.data.release.utils.ExecutionUtils;
@@ -59,6 +60,7 @@ import org.springframework.util.AntPathMatcher;
 
 /**
  * @author Mark Paluch
+ * @author Chris Bono
  */
 @CliComponent
 @RequiredArgsConstructor
@@ -77,6 +79,28 @@ public class LicenseHeaderCommands extends TimedCommand {
 
 	List<String> filePatterns = Arrays.asList("pom.xml", "**/*.java", "**/*.kt", "**/*.adoc");
 
+	enum FileType {
+
+		POM_XML, JAVA, KOTLIN, ADOC;
+
+		static FileType of(File file) {
+			var fileName = file.getName().toLowerCase(Locale.ROOT);
+			if (fileName.equals("pom.xml")) {
+				return FileType.POM_XML;
+			}
+			if (fileName.endsWith(".adoc")) {
+				return FileType.ADOC;
+			}
+			if (fileName.endsWith(".java")) {
+				return FileType.JAVA;
+			}
+			if (fileName.endsWith(".kt")) {
+				return FileType.KOTLIN;
+			}
+			throw new IllegalArgumentException("Unknown file type: " + file.getName());
+		}
+	}
+
 	/**
 	 * Process all files matching {@link #filePatterns} and update the Apache license header year range, extending to
 	 * {@code year}. Rewrites single-year and year-range formats.
@@ -86,7 +110,7 @@ public class LicenseHeaderCommands extends TimedCommand {
 	 */
 	@CliCommand(value = "update license-headers")
 	public void updateLicenseHeaders(@CliOption(key = "", mandatory = true) TrainIteration iteration,
-			@CliOption(key = "year", mandatory = true) int year,
+			@CliOption(key = "year", mandatory = true) String year,
 			@CliOption(key = "project", mandatory = false) String projectName) {
 
 		git.prepare(iteration);
@@ -95,7 +119,7 @@ public class LicenseHeaderCommands extends TimedCommand {
 
 		if (projectName != null) {
 			Project project = Projects.requiredByName(projectName);
-			modules = modules.filter(it -> it.getSupportedProject().equals(project));
+			modules = modules.filter(it -> it.getSupportedProject().getProject().equals(project));
 		}
 
 		ExecutionUtils.run(executor, modules, module -> {
@@ -108,27 +132,44 @@ public class LicenseHeaderCommands extends TimedCommand {
 		});
 	}
 
-	private int updateLicense(int year, ModuleIteration module) {
+	private int updateLicense(String year, ModuleIteration module) {
+		return replaceInFiles(module.getSupportedProject(), (file, content) ->
+				this.updateLicenseHeaderInFile(FileType.of(file), content, year, module.getSupportStatus()));
+	}
 
-		return replaceInFiles(module.getSupportedProject(), (file, content) -> {
+	static String updateLicenseHeaderInFile(FileType fileType, String content, String year, SupportStatus supportStatus) {
+		String contentToUse = content;
 
-			String contentToUse = content;
+		if (fileType == FileType.ADOC) {
+			return content.replaceAll("([\\d]{4})-([\\d]{4})",
+					"$1-" + year);
+		}
 
-			String filename = file.getName().toLowerCase(Locale.ROOT);
-			if (filename.endsWith(".adoc")) {
-				return content.replaceAll("([\\d]{4})-([\\d]{4})", "$1-" + year);
+		contentToUse = contentToUse.replaceAll("(C) ([\\d]{4})-([\\d]{4})",
+				"(C) $1-" + year);
+
+ 		contentToUse = contentToUse.replaceAll("Copyright ([\\d]{4}) the original author or authors",
+				"Copyright $1-" + year + " the original author or authors");
+
+		contentToUse = contentToUse.replaceAll("Copyright ([\\d]{4})-([\\d]{4}) the original author or authors",
+				"Copyright $1-" + year + " the original author or authors");
+
+		if (supportStatus.isCommercial() && fileType != FileType.POM_XML) {
+			var broadcomCopyright = "Broadcom Inc. and/or its subsidiaries. All Rights Reserved.";
+			if (contentToUse.contains(broadcomCopyright)) {
+				return contentToUse;
 			}
 
-			contentToUse = contentToUse.replaceAll("(C) ([\\d]{4})-([\\d]{4})", "(C) $1-" + year);
+			// insert Broadcom header above original header
+			contentToUse = contentToUse.replaceAll("(.*)Copyright ([\\d]{4})(.*)",
+					"$1Copyright $2 Broadcom Inc. and/or its subsidiaries. All Rights Reserved.\n$1Copyright $2$3");
 
-			contentToUse = contentToUse.replaceAll("Copyright ([\\d]{4}) the original author or authors",
-					"Copyright $1-" + year + " the original author or authors");
+			// chop Apache header out while preserving line numbers
+			contentToUse = contentToUse.replaceAll("(.*)(?s)\\sLicensed under the Apache License.*limitations under the License\\.(.*)",
+					"$1%s$2".formatted("\n\s*".repeat(9)));
+		}
 
-			contentToUse = contentToUse.replaceAll("Copyright ([\\d]{4})-([\\d]{4}) the original author or authors",
-					"Copyright $1-" + year + " the original author or authors");
-
-			return contentToUse;
-		});
+		return contentToUse;
 	}
 
 	private void commitAndPushWithTicket(ModuleIteration module, String ticketSummary) throws InterruptedException {

src/test/java/org/springframework/data/release/infra/LicenseHeaderCommandsUnitTests.java
@@ -1,0 +1,245 @@
+/*
+ * Copyright 2014-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.release.infra;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.release.infra.LicenseHeaderCommands.FileType;
+import org.springframework.data.release.model.SupportStatus;
+
+/**
+ * Unit tests for {@link LicenseHeaderCommands}.
+ *
+ * @author Chris Bono
+ */
+class LicenseHeaderCommandsUnitTests {
+
+	private LicenseHeaderCommands commands;
+
+	@BeforeEach
+	void prepareForTest() {
+		commands = new LicenseHeaderCommands(mock(), mock(), mock(), mock(), mock());
+	}
+
+	@Nested
+	class WithOpenSourceFilesTests {
+
+		@Test
+		void javaHeaderWithStartDateOnlyUpdatedToPresent() {
+			var originalContent = openSourceJavaFile("2022");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.JAVA, originalContent, "present",
+					SupportStatus.OSS);
+			var expectedContent = openSourceJavaFile("2022-present");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+
+		@Test
+		void javaHeaderWithStartAndEndDateUpdatedToPresent() {
+			var originalContent = openSourceJavaFile("2022-2024");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.JAVA, originalContent, "present",
+					SupportStatus.OSS);
+			var expectedContent = openSourceJavaFile("2022-present");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+
+		@Test
+		void javaHeaaderUpdateIsIdempotent() {
+			var originalContent = openSourceJavaFile("2022-2024");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.JAVA, originalContent, "present",
+					SupportStatus.OSS);
+			updatedContent = commands.updateLicenseHeaderInFile(FileType.JAVA, updatedContent, "present", SupportStatus.OSS);
+			var expectedContent = openSourceJavaFile("2022-present");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+
+		@Test
+		void adocWithStartAndEndDateUpdatedToPresent() {
+			var originalContent = adocFile("2022-2024");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.ADOC, originalContent, "present",
+					SupportStatus.OSS);
+			var expectedContent = adocFile("2022-present");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+
+		@Test
+		void pomXmlWithStartAndEndDateUpdatedToPresent() {
+			var originalContent = pomFile("2022-2024");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.POM_XML, originalContent, "present",
+					SupportStatus.OSS);
+			var expectedContent = pomFile("2022-present");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+	}
+
+	@Nested
+	class WithCommercialJavaFilesTests {
+
+		@Test
+		void javaHeaderWithStartDateOnlyUpdatedToPresent() {
+			var originalContent = openSourceJavaFile("2022");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.JAVA, originalContent, "present",
+					SupportStatus.COMMERCIAL);
+			var expectedContent = commercialJavaFile("2022");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+
+		@Test
+		void javaHeaderWithStartAndEndDateUpdatedToPresent() {
+			var originalContent = openSourceJavaFile("2022-2024");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.JAVA, originalContent, "present",
+					SupportStatus.COMMERCIAL);
+			var expectedContent = commercialJavaFile("2022");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+
+		@Test
+		void javaHeaderUpdateIsIdempotent() {
+			var originalContent = openSourceJavaFile("2022-2024");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.JAVA, originalContent, "present",
+					SupportStatus.COMMERCIAL);
+			updatedContent = commands.updateLicenseHeaderInFile(FileType.JAVA, updatedContent, "present",
+					SupportStatus.COMMERCIAL);
+			var expectedContent = commercialJavaFile("2022");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+
+		@Test
+		void adocWithStartAndEndDateUpdatedToPresent() {
+			var originalContent = adocFile("2022-2024");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.ADOC, originalContent, "present",
+					SupportStatus.COMMERCIAL);
+			var expectedContent = adocFile("2022-present");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+
+		@Test
+		void pomXmlWithStartAndEndDateUpdatedToPresent() {
+			var originalContent = pomFile("2022-2024");
+			var updatedContent = commands.updateLicenseHeaderInFile(FileType.POM_XML, originalContent, "present",
+					SupportStatus.COMMERCIAL);
+			var expectedContent = pomFile("2022-present");
+			assertThat(updatedContent).isEqualTo(expectedContent);
+		}
+
+	}
+
+	private String openSourceJavaFile(String dateRange) {
+		return """
+				/*
+				 * Copyright %s the original author or authors.
+				 *
+				 * Licensed under the Apache License, Version 2.0 (the "License");
+				 * you may not use this file except in compliance with the License.
+				 * You may obtain a copy of the License at
+				 *
+				 *      https://www.apache.org/licenses/LICENSE-2.0
+				 *
+				 * Unless required by applicable law or agreed to in writing, software
+				 * distributed under the License is distributed on an "AS IS" BASIS,
+				 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+				 * See the License for the specific language governing permissions and
+				 * limitations under the License.
+				 */
+				package some.package;
+				
+				import some.otherpackage.Foo;
+				
+				/**
+				 * This is some class comment.
+				 */
+				public class SomeClass {
+				}""".formatted(dateRange);
+	}
+
+	private String commercialJavaFile(String startDate) {
+		return """
+				/*
+				 * Copyright %s Broadcom Inc. and/or its subsidiaries. All Rights Reserved.
+				 * Copyright %s-present the original author or authors.
+				 *
+				 *
+				 *
+				 *
+				 *
+				 *
+				 *
+				 *
+				 *
+				 *
+				 *
+				 */
+				package some.package;
+				
+				import some.otherpackage.Foo;
+				
+				/**
+				 * This is some class comment.
+				 */
+				public class SomeClass {
+				}""".formatted(startDate, startDate);
+	}
+
+	private String adocFile(String dateRange) {
+		return """
+Some adoc content.
+
+(C) 2008-{copyright-year} VMware, Inc.
+
+Copies of this document may be made for your own use and for distribution to others
+				""";
+	}
+
+	private String pomFile(String dateRange) {
+		return """
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <name>Spring Data Release Train - BOM Infrastructure</name>
+    <licenses>
+        <license>
+            <name>Apache License, Version 2.0</name>
+            <url>https://www.apache.org/licenses/LICENSE-2.0</url>
+            <comments>
+                Copyright %s the original author or authors.
+
+                Licensed under the Apache License, Version 2.0 (the "License");
+                you may not use this file except in compliance with the License.
+                You may obtain a copy of the License at
+
+                https://www.apache.org/licenses/LICENSE-2.0
+
+                Unless required by applicable law or agreed to in writing, software
+                distributed under the License is distributed on an "AS IS" BASIS,
+                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+                implied.
+                See the License for the specific language governing permissions and
+                limitations under the License.
+            </comments>
+        </license>
+    </licenses>
+    <profiles>
+        <profile>
+            <id>some-profile</id>
+        </profile>
+    </profiles>
+</project>
+		""".formatted(dateRange);
+	}
+
+}

commit 6bab15e4594e49a0ec49aa440cd7e05c4b9db429

Fix commit message for Broadcom license header update
This fixes an issue introduced in commit e0dbf5e4ba38cd0ef5f6a9f9cd1919eb32c6ce1d where the format string for the commit message now requires a string for the year parameter.

Also, the commit message is extended to mention the Broadcom license header update when the module is commercially supported.

Signed-off-by: Chris Bono <chris.bono@broadcom.com>

src/main/java/org/springframework/data/release/infra/LicenseHeaderCommands.java
@@ -106,7 +106,11 @@ public class LicenseHeaderCommands extends TimedCommand {
 			int updated = updateLicense(year, module);
 
 			if (updated > 0) {
-				commitAndPushWithTicket(module, String.format("Extend license header copyright years to %d", year));
+				String commitMsg = String.format("Extend license header copyright years to %s", year);
+				if (module.getSupportStatus() == SupportStatus.COMMERCIAL) {
+					commitMsg = commitMsg + " and replace Apache 2 with Broadcom license";
+				}
+				commitAndPushWithTicket(module, commitMsg);
 			}
 		});
 	}